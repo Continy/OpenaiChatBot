a6dd3cc42070252eb17d219bf0fb23fb18d48b80
Continy <satanama.ring@gmail.com>
2022-12-09 10:07:37 +0800

    去除了chat功能，等马氏服务器修好了再开放

--- bot.py
+++ bot.py
@@ -16,6 +16,11 @@ ansSet = []
 settingSet = ''
 
 
+def addSetting(new):
+    global settingSet
+    settingSet = settingSet + '\n' + new
+
+
 def generateInput(askSet, ansSet, settingSet):
     output = settingSet + '\n'
     for i in range(0, len(askSet) - 1):
@@ -46,62 +51,71 @@ async def group_message_listener(app: Ariadne, group: Group,
     global askSet
     global ansSet
     global settingSet
-    print(message.display)
+
     at_1 = [r.span() for r in re.finditer('@3475', message.display)]
     at_2 = [r.span() for r in re.finditer('Bot', message.display)]
-    print(at_2)
+
     if at_1 or at_2:
         if at_1:
             message = message.display[11:]
         else:
-            message = message.display[14:]
+            message = message.display[4:]
         img_index = [r.span() for r in re.finditer('生成', message)]
-        chat_index = [r.span() for r in re.finditer('/chat', message)]
-        print(img_index)
+        chat_index = [r.span() for r in re.finditer('-', message)]
         #await app.send_message(Group, MessageChain([Plain("别急，停机维护中")]))
         if chat_index:
-            if chatMode:
+            mode1 = [r.span() for r in re.finditer('1', message)]
+            modeAdd = [r.span() for r in re.finditer('add', message)]
+            mode0 = [r.span() for r in re.finditer('0', message)]
+            if chatMode and mode0:
                 chatMode = 0
                 askSet = []
                 ansSet = []
                 settingSet = ''
-                await app.send_message(group,
-                                       MessageChain([Plain('chatMode = 0')]))
-            else:
+                await app.send_message(group, MessageChain([Plain('-0')]))
+            elif mode1:
                 chatMode = 1
-                settingSet = message[6:]
+                settingSet = message[2:]
                 await app.send_message(
-                    group, MessageChain([Plain('chatMode = 1 ' + settingSet)]))
+                    group, MessageChain([Plain('-1' + settingSet)]))
+            elif modeAdd:
+                chatMode = 1
+                settingSet = message[4:]
+                addSetting(settingSet)
+                await app.send_message(
+                    group, MessageChain([Plain('-add' + settingSet)]))
 
         elif img_index:
             inputStr = message[3:]
-            print(inputStr)
-            # response = openai.Image.create(prompt=inputStr, n=1, size="256x256")
-            # image_url = response['data'][0]['url']
-            # await app.send_message(group, MessageChain([Plain(image_url)]))
+            response = openai.Image.create(prompt=inputStr,
+                                           n=1,
+                                           size="512x512")
+            image_url = response['data'][0]['url']
+            await app.send_message(group, MessageChain([Plain(image_url)]))
 
+        # else:
+        #     print("#######################")
+        # if chatMode:
+        #     askSet.append(message)
+        #     print(generateInput(askSet, ansSet, settingSet))
+        #     response = openai.Completion.create(model="text-davinci-003",
+        #                                         prompt=generateInput(
+        #                                             askSet, ansSet,
+        #                                             settingSet),
+        #                                         temperature=0.9,
+        #                                         max_tokens=150,
+        #                                         top_p=1,
+        #                                         frequency_penalty=0,
+        #                                         presence_penalty=0.6)
+        #     result = response['choices'][0]['text']
+        #     ansSet.append(result)
+        #     await app.send_message(group, MessageChain([Plain(result)]))
         else:
-            print("#######################")
-            if chatMode:
-                askSet.append(message)
-                print(generateInput(askSet, ansSet, settingSet))
-                response = openai.Completion.create(model="text-davinci-003",
-                                                    prompt=generateInput(
-                                                        askSet, ansSet,
-                                                        settingSet),
-                                                    temperature=0.9,
-                                                    max_tokens=300,
-                                                    top_p=1,
-                                                    frequency_penalty=0,
-                                                    presence_penalty=0.6)
-                result = response['choices'][0]['text']
-                ansSet.append(result)
-                await app.send_message(group, MessageChain([Plain(result)]))
-            else:
+            try:
                 response = openai.Completion.create(model="text-davinci-003",
                                                     prompt=message,
                                                     temperature=0.3,
-                                                    max_tokens=1024,
+                                                    max_tokens=250,
                                                     top_p=1,
                                                     frequency_penalty=0,
                                                     presence_penalty=0)
@@ -109,6 +123,9 @@ async def group_message_listener(app: Ariadne, group: Group,
                 all_index = [r.span() for r in re.finditer('\n', result)]
                 await app.send_message(
                     group, MessageChain([Plain(result[all_index[1][1]:])]))
+            except:
+                await app.send_message(group,
+                                       MessageChain([Plain("什么情况？死机了！")]))
 
 
 app.launch_blocking()
